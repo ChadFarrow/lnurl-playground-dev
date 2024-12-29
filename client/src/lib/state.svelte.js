d21aed9bbf87885cea5741a4959fac8d0b60d792
Steven <dev.thebells1111@gmail.com>
2024-12-29 14:29:48 -0600

    add TSB to remoter server route

--- client/src/lib/state.svelte.js
+++ client/src/lib/state.svelte.js
@@ -1,6 +1,8 @@
 import { dev } from "$app/environment";
 
-export const remoteServer = dev ? "http://localhost:3000" : "";
+export const remoteServer = dev
+  ? "http://localhost:3000"
+  : "https://thesplitbox.com";
 export const albyClientId = dev ? "hzopQVxGtD" : "3IsGwlTSvi";
 export const albyRedirectUrl = dev
   ? "http://localhost:5173"
