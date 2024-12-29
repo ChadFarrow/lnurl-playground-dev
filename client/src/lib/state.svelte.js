87f9c081ca20c04cf7b9584c2df42cfb3a8e61e0
Steven <dev.thebells1111@gmail.com>
2024-12-29 14:11:31 -0600

    rebuild with updated redirect uri

--- client/src/lib/state.svelte.js
+++ client/src/lib/state.svelte.js
@@ -2,7 +2,9 @@ import { dev } from "$app/environment";
 
 export const remoteServer = dev ? "http://localhost:3000" : "";
 export const albyClientId = dev ? "hzopQVxGtD" : "3IsGwlTSvi";
-export const albyRedirectUrl = dev ? "http://localhost:5173" : "";
+export const albyRedirectUrl = dev
+  ? "http://localhost:5173"
+  : "https://thesplitbox.com";
 
 export const user = $state({
   address: "",
