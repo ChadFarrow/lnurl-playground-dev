86a7135630ef91f979cc93b8250d0d79530d48d8
Steven <dev.thebells1111@gmail.com>
2024-12-29 20:05:03 -0600

    clean up public folder

--- server/public/_app/immutable/chunks/misc.Ivegw3xs.js
+++
@@ -1,1 +0,0 @@
-import{s,g as e,a as n}from"./runtime.BAIukHDA.js";function u(t){var r=n(0);return function(){return arguments.length===1?(s(r,e(r)+1),arguments[0]):(e(r),t())}}export{u as r};
--- server/public/_app/immutable/entry/start.BTG9U6Jx.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.Bani7yVG.js";export{t as start};
