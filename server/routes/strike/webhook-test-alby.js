c1bff7a3eadc7b2f38292d4a29af106bbfa0d5a2
Steven Bell <dev.thebells1111@gmail.com>
2025-01-08 21:05:29 -0600

    more fixes to webhook routes

--- server/routes/strike/webhook-test-alby.js
+++ server/routes/strike/webhook-test-alby.js
@@ -1,32 +1,31 @@
 import fs from "fs/promises";
 
-async function webhookTest(req, res) {
-  const filePath = "alby-webhook-test.json";
+async function webhookTestAlby() {
+  return async (req, res) => {
+    const filePath = "alby-webhook-test.json";
 
-  try {
-    // Read existing file
-    let content = [];
     try {
-      const data = await fs.readFile(filePath, "utf-8");
-      if (data.length) {
-        content = JSON.parse(data);
+      // Read existing file
+      let content = [];
+      try {
+        const data = await fs.readFile(filePath, "utf-8");
+        if (data.length) {
+          content = JSON.parse(data);
+        }
+      } catch (err) {
+        if (err.code !== "ENOENT") throw err; // Ignore file-not-found errors
       }
+
+      // Append new request body
+      content.push(req.body);
+
+      // Write updated array back to file
+      await fs.writeFile(filePath, JSON.stringify(content, null, 2));
+      res.send("Webhook received and saved");
     } catch (err) {
-      if (err.code !== "ENOENT") throw err; // Ignore file-not-found errors
+      console.error(err);
+      res.status(500).send("Failed to process the request");
     }
-
-    // Append new request body
-    content.push(req.body);
-
-    // Write updated array back to file
-    await fs.writeFile(filePath, JSON.stringify(content, null, 2));
-    res.send("Webhook received and saved");
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Failed to process the request");
-  }
+  };
 }
-
-export default webhookTest;
-
-("alby-webhook-test.json");
+export default webhookTestAlby;
