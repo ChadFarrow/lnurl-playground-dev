3a409ef0e67d5086f25e4076463c554191ee3531
Steven Bell <dev.thebells1111@gmail.com>
2025-01-09 09:31:09 -0600

    add dynamic route to wellknown

--- server/routes/wellknown/wellknownRoutes.js
+++ server/routes/wellknown/wellknownRoutes.js
@@ -22,5 +22,28 @@ router.get("/lnurlp/prism", (req, res) => {
   });
 });
 
+router.get("/lnurlp/:name", (req, res) => {
+  const { name } = req.params; // Extract the dynamic part from the route
+  console.log(name); // Log the dynamic name
+
+  res.json({
+    status: "OK",
+    tag: "payRequest",
+    commentAllowed: 255,
+    callback: `https://getalby.com/lnurlp/prism/callback#${name}`, // Use the dynamic name
+    metadata: `[["text/identifier","prism@getalby.com"],["text/plain","Sats for ${name}"]]`,
+    minSendable: 1000,
+    maxSendable: 10000000000,
+    payerData: {
+      name: { mandatory: false },
+      email: { mandatory: false },
+      pubkey: { mandatory: false },
+    },
+    nostrPubkey:
+      "23103189356cf7c8bc09bb8b431fc3e71e85582c8f755b9ee160203c9c19e403",
+    allowsNostr: true,
+  });
+});
+
 const wellknownRoutes = router;
 export default wellknownRoutes;
